5fa933547bce837875d43c0174aa847b7260c64c
Eric Brandes <eric.brandes@gmail.com>
2017-07-24 08:53:59 -0500

    initial project structure

---
+++ device/device.ts
@@ -0,0 +1,5 @@
+declare var autowatch: number;
+declare var post: any;
+autowatch = 1;
+
+console.log("eric")
---
+++ device/log.ts
@@ -0,0 +1,19 @@
+function log() {
+  for(var i=0,len=arguments.length; i<len; i++) {
+    var message = arguments[i];
+    if(message && message.toString) {
+      var s = message.toString();
+      if(s.indexOf("[object ") >= 0) {
+        s = JSON.stringify(message);
+      }
+      post(s);
+    }
+    else if(message === null) {
+      post("<null>");
+    }
+    else {
+      post(message);
+    }
+  }
+  post("\n");
+}
