3721b2c28626e9362ef260312ccf0ddaab904bb1
Eric Brandes <eric.brandes@gmail.com>
2018-03-31 21:02:10 -0500

    Removing old types

--- dist/models/note.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class Note {
-    pitch: number;
-    time: number;
-    duration: number;
-    velocity: number;
-    muted: boolean;
-    constructor(pitch: number, time: number, duration: number, velocity?: number, muted?: boolean);
-    toNoteArgumentFormat(): any[];
-}
--- dist/models/track.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class Track {
-    path: string;
-    name: string;
-    isMidi: boolean;
-    isAudio: boolean;
-    constructor(path: string, name: string, isMidi: boolean);
-}
